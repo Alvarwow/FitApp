22fcc4206dc1e21ed84cc26b3df64ae6b68e1e9e
Alvarwow <alvaroherencias@hotmail.com>
2021-02-19 21:36:07 +0100

    commit

--- app/src/main/java/com/example/fitapp/MainActivity.java
+++ app/src/main/java/com/example/fitapp/MainActivity.java
@@ -9,25 +9,18 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Toast;
-
 import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
-import com.google.firebase.firestore.DocumentReference;
-import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.QueryDocumentSnapshot;
-import com.google.firebase.firestore.QuerySnapshot;
-
-import java.util.HashMap;
-import java.util.Map;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     private static final String TAG = "MainActivity";
     private EditText email;
     private EditText pass;
-   private FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private FirebaseAuth mAuth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -36,22 +29,80 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         login.setOnClickListener(this);
         Button register = findViewById(R.id.bRegister);
         register.setOnClickListener(this);
+        mAuth = FirebaseAuth.getInstance();
+
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
 
 
     }
+   public void createAcount(){
+       email = findViewById(R.id.eEmail);
+       pass = findViewById(R.id.ePass);
+
+       mAuth.createUserWithEmailAndPassword(email.getText().toString(), pass.getText().toString())
+               .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                   @Override
+                   public void onComplete(@NonNull Task<AuthResult> task) {
+                       if (task.isSuccessful()) {
+                           // Sign in success, update UI with the signed-in user's information
+                           Log.d(TAG, "createUserWithEmail:success");
+                           FirebaseUser user = mAuth.getCurrentUser();
+
+                       } else {
+                           // If sign in fails, display a message to the user.
+                           Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                           //Toast.makeText(EmailPasswordActivity.this, "Authentication failed.",
+                                //   Toast.LENGTH_SHORT).show();
+
+                       }
+
+                       // ...
+                   }
+               });
+
+   }
+   public void login(){
+       email = findViewById(R.id.eEmail);
+       pass = findViewById(R.id.ePass);
+       mAuth.signInWithEmailAndPassword(email.getText().toString(), pass.getText().toString())
+               .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                   @Override
+                   public void onComplete(@NonNull Task<AuthResult> task) {
+                       if (task.isSuccessful()) {
+                           // Sign in success, update UI with the signed-in user's information
+                           Log.d(TAG, "signInWithEmail:success");
+                           FirebaseUser user = mAuth.getCurrentUser();
+                           lanzarIntent();
+                       } else {
+                           // If sign in fails, display a message to the user.
+                           Log.w(TAG, "signInWithEmail:failure", task.getException());
+
+
+                       }
+
+                       // ...
+                   }
+               });
+    }
+
+
 
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.bLogin:
 
-                comprobarUsuario();
-
-
+login();
                 break;
             case R.id.bRegister:
 
-                registrarse();
+                createAcount();
 
 
                 break;
@@ -59,77 +110,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     public void lanzarIntent() {
-        Intent intent = new Intent(this,Home.class);
-        intent.putExtra("email",email.getText().toString());
-        intent.putExtra("pass",pass.getText().toString());
+        Intent intent = new Intent(this, Home.class);
+        intent.putExtra("email", email.getText().toString());
+        intent.putExtra("pass", pass.getText().toString());
         startActivity(intent);
         finish();
     }
 
-    public void comprobarUsuario(){
-
-        email = findViewById(R.id.eEmail);
-        pass = findViewById(R.id.ePass);
-        db.collection("usuarios")
-                .whereEqualTo("email", email.getText().toString()).whereEqualTo("pass",pass.getText().toString())
-                .get()
-                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-                    @Override
-                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
-                        boolean usuarioOk=false;
-                        boolean passOk=false;
-                        if (task.isSuccessful()) {
-                            for (QueryDocumentSnapshot document : task.getResult()) {
-
-                                if(document.getId().equals("email")&&document.getData().equals(email.getText().toString())){
-                                    usuarioOk=true;
-                                }
-                                if(document.getId().equals("pass")&&document.getData().equals(pass.getText().toString())){
-                                    passOk=true;
-                                }
-                            }
-
-if(usuarioOk&&passOk){
-   lanzarIntent();
-}else{
-    Log.d(TAG, "No es correcto");
-}
-                        }
-                    }
-                });
-
-    }
-    public void registrarse() {
-        email = findViewById(R.id.eEmail);
-        pass = findViewById(R.id.ePass);
-        if (!email.getText().toString().equals("") && !pass.getText().toString().equals("")) {
 
 
-
-            // Create a new user with a first and last name
-            Map<String, Object> usuario = new HashMap<>();
-
-            usuario.put("email", email.getText().toString());
-            usuario.put("pass", pass.getText().toString());
-
-
-// Add a new document with a generated ID
-            db.collection("usuarios")
-                    .add(usuario)
-                    .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
-                        @Override
-                        public void onSuccess(DocumentReference documentReference) {
-                            Log.d(TAG, "Usuario insertado correctamente");
-                        }
-                    })
-                    .addOnFailureListener(new OnFailureListener() {
-                        @Override
-                        public void onFailure(@NonNull Exception e) {
-                            Log.w(TAG, "Error");
-                        }
-                    });
-        } else {
-            Toast.makeText(this, "Debes introducir email y contraseña", Toast.LENGTH_SHORT).show();
-        }
-    }
 }
