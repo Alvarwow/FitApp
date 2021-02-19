9fdd9354e65b8ce1353de126b26767d74eee7e7d
Alvarwow <alvaroherencias@hotmail.com>
2021-02-19 23:03:09 +0100

    login funcional y añade datos

--- app/src/main/java/com/example/fitapp/Home.java
+++ app/src/main/java/com/example/fitapp/Home.java
@@ -3,12 +3,47 @@ package com.example.fitapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class Home extends AppCompatActivity {
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
+public class Home extends AppCompatActivity implements View.OnClickListener {
+    private DatabaseReference mDatabase;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        Button annadirEjercicio=findViewById(R.id.bAnnadir);
+        annadirEjercicio.setOnClickListener(this);
+
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.bAnnadir:
+                nuevoEjercicio();
+                return;
+        }
+    }
+
+
+    public void nuevoEjercicio(){
+        ejercicio e=new ejercicio();
+        e.setNombre("Flexiones");
+        e.setRepeticiones(25);
+        e.setSeries(4);
+        e.setVideo("hola");
+
+        // Write a message to the database
+        mDatabase = FirebaseDatabase.getInstance("https://fitapp-1ca9d-default-rtdb.europe-west1.firebasedatabase.app/").getReference("ejercicio");
+
+        mDatabase.push().setValue(e);
+
+
+        System.out.println("e introducido");
     }
 }
---
+++ app/src/main/java/com/example/fitapp/ejercicio.java
@@ -0,0 +1,52 @@
+package com.example.fitapp;
+
+public class ejercicio {
+
+    private String nombre;
+    private int repeticiones;
+    private int series;
+    private String video;
+
+    public ejercicio(String nombre, int repeticiones, int series, String video) {
+        this.nombre = nombre;
+        this.repeticiones = repeticiones;
+        this.series = series;
+        this.video = video;
+    }
+    public ejercicio() {
+
+    }
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getRepeticiones() {
+        return repeticiones;
+    }
+
+    public void setRepeticiones(int repeticiones) {
+        this.repeticiones = repeticiones;
+    }
+
+    public int getSeries() {
+        return series;
+    }
+
+    public void setSeries(int series) {
+        this.series = series;
+    }
+
+    public String getVideo() {
+        return video;
+    }
+
+    public void setVideo(String video) {
+        this.video = video;
+    }
+
+
+}
